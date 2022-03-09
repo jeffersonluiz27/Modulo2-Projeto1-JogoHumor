400854ead25ae217eca5442c0ade86e021d9a070
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-03-08 22:14:42 -0300

    reduzindo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,27 @@
 const getElementoNome = document.getElementById('humor');
-const getElementoImg = document.querySelector('#imgMain');
-const getElementoCor = document.querySelector('#content');
+const getElementoImg = document.getElementById('imgMain');
+const getElementoCor = document.getElementById('content');
 let getElementoBtn = document.querySelector('#butonChange');
 
 getElementoBtn.addEventListener('click', () => {
 	if (getElementoBtn.value == '1') {
-		getElementoNome.innerText = 'Timido';
-		getElementoImg.src = './assets/img/Embarrassed.png';
-		getElementoCor.style.backgroundColor = '#81f298';
-		getElementoBtn.value = '2';
+		mudaAtrub('Timido','./assets/img/Embarrassed.png','#81f298','2');
 	} else if (getElementoBtn.value == '2') {
-		getElementoNome.innerText = 'Apaixonado';
-		getElementoImg.src = './assets/img/InLove.png';
-		getElementoCor.style.backgroundColor = '#f3b6c6';
-		getElementoBtn.value = '3';
+		mudaAtrub('Apaixonado','./assets/img/InLove.png','#f3b6c6','3');
 	} else if (getElementoBtn.value == '3') {
-		getElementoNome.innerText = 'Divertido';
-		getElementoImg.src = './assets/img/Funny.png';
-		getElementoCor.style.backgroundColor = '#f79554';
-		getElementoBtn.value = '4';
+		mudaAtrub('Divertido','./assets/img/Funny.png','#f79554','4');
 	} else if (getElementoBtn.value == '4') {
-		getElementoNome.innerText = 'Bravo';
-		getElementoImg.src = './assets/img/Angry.png';
-		getElementoCor.style.backgroundColor = '#dd6070';
-		getElementoBtn.value = '5';
+		mudaAtrub('Bravo','./assets/img/Angry.png','#dd6070','5');
 	} else if (getElementoBtn.value == '5') {
-		getElementoNome.innerText = 'Triste';
-		getElementoImg.src = './assets/img/Sad.png';
-		getElementoCor.style.backgroundColor = '#82c3eb';
-		getElementoBtn.value = '6';
+		mudaAtrub('Triste','./assets/img/Sad.png','#82c3eb','6');
 	} else {
-		getElementoNome.innerText = 'Alegre';
-		getElementoImg.src = './assets/img/Happy.png';
-		getElementoCor.style.backgroundColor = '#fdd900';
-		getElementoBtn.value = '1';
+		mudaAtrub('Alegre','./assets/img/Happy.png','#fdd900','1');
 	}
 });
+
+function mudaAtrub(humor, image,color,valor){
+	getElementoNome.innerText = humor;
+	getElementoImg.src = image;
+	getElementoCor.style.backgroundColor = color;
+	getElementoBtn.value = valor;
+}
